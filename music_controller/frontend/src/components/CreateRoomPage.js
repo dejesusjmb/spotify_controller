ebe9ef6c3b3471d4c9d75167cbbfc9e2a8944fd9
JM De Jesus <jdejesus@buildzoom.com>
2022-01-31 21:42:44 +0800

    Initial project commit

---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class CreateRoomPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>this is the create room page</h1>
+    }
+
+}
---
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class RoomJoinPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>this is the room join page</h1>
+    }
+
+}
